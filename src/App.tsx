50fe729cdf35abf9bb23ffef7703bbe2bb0bdf43
devmhs <mhs.02@hotmail.com>
2024-12-25 14:15:05 -0300

    Atualizações no layout e correção de erros no código

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './App.css';
 
 function App() {
   const [participantes, setParticipantes] = useState("");
@@ -27,39 +28,48 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Gerador de Times</h1>
-      <textarea
-        rows="6"
-        placeholder="Digite os nomes dos participantes (um por linha)"
-        value={participantes}
-        onChange={(e) => setParticipantes(e.target.value)}
-        style={{ width: "100%", marginBottom: "10px" }}
-      />
-      <input
-        type="number"
-        min="2"
-        value={numeroDeTimes}
-        onChange={(e) => setNumeroDeTimes(Number(e.target.value))}
-        style={{ marginBottom: "10px", width: "100%" }}
-      />
-      <button onClick={handleGerarTimes} style={{ marginBottom: "20px" }}>
-        Gerar Times
-      </button>
-      {times.length > 0 && (
-        <div>
-          {times.map((time, index) => (
-            <div key={index}>
-              <h3>Time {index + 1}</h3>
-              <ul>
-                {time.map((p: string, i: number) => (
-                  <li key={i}>{p}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="container">
+      <div className="main-content">
+        <h1>Gerador de Times</h1>
+        <textarea
+          rows={6}
+          placeholder="Digite os nomes dos participantes (um por linha)"
+          value={participantes}
+          onChange={(e) => setParticipantes(e.target.value)}
+          className="textarea"
+        />
+        <input
+          type="number"
+          min="2"
+          value={numeroDeTimes}
+          onChange={(e) => setNumeroDeTimes(Number(e.target.value))}
+          className="input"
+        />
+        <button onClick={handleGerarTimes} className="button">
+          Gerar Times
+        </button>
+
+        {times.length > 0 && (
+          <div className="times-container">
+            
+            {times.map((time, index) => (
+              <div key={index} className="time-item">
+                <h3>Time {index + 1} </h3>
+                <ul>
+                  {time.map((p: string, i: number) => (
+                    <li key={i}>{p}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Rodapé */}
+      <p className="footer">
+        Desenvolvido por <a href="https://github.com/MarcosHenriquedaSilva" target="_blank" rel="noopener noreferrer">MHS - Dev</a>
+      </p>
     </div>
   );
 }
